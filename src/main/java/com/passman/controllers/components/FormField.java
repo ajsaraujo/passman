a52db01a45c4a6cda31dac2bac7b08b5f58e857a
Allan <allanjuansil@gmail.com>
2020-09-21 19:41:44 -0300

    Add fields to sign up view and fix compiler settings

--- src/main/java/com/passman/controllers/components/FormField.java
+++ src/main/java/com/passman/controllers/components/FormField.java
@@ -2,10 +2,31 @@ package com.passman.controllers.components;
 
 import com.passman.enums.Component;
 import com.passman.utils.FileUtils;
+import javafx.beans.NamedArg;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class FormField extends AnchorPane {
-    public FormField() {
+    @FXML Label label;
+    @FXML TextField textField;
+
+    private final String labelText;
+
+    public FormField(@NamedArg("labelText") String labelText) {
+        this.labelText = labelText;
+
         FileUtils.injectComponentController(Component.FORM_FIELD, this);
+    }
+
+    @FXML
+    public void initialize() {
+        label.setText(labelText);
+        textField.setText(null);
+    }
+
+    public String getLabelText() {
+        return labelText;
     }
 }
