54acfb8a98054f7aee8c6fbc853b88a8990a2f6d
Allan <allanjuansil@gmail.com>
2020-10-06 15:40:36 -0300

    Continue to login screen when button is clicked

--- src/main/java/com/passman/ui/views/SignUpSuccess.java
+++ src/main/java/com/passman/ui/views/SignUpSuccess.java
@@ -1,7 +1,12 @@
 package com.passman.ui.views;
 
+import com.passman.commons.View;
 import com.passman.commons.abstracts.ViewController;
+import javafx.fxml.FXML;
 
 public class SignUpSuccess extends ViewController {
-
+    @FXML
+    public void continueButtonClicked() {
+        navigator.pushReplacement(new View("login"));
+    }
 }
