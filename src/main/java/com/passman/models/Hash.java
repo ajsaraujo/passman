375a556739186e13941750248186cd4380a3c346
Allan <allanjuansil@gmail.com>
2020-10-07 15:25:31 -0300

    Refactor to byte[] and implement verify method

--- src/main/java/com/passman/models/Hash.java
+++ src/main/java/com/passman/models/Hash.java
@@ -1,24 +1,61 @@
 package com.passman.models;
 
-public class Hash {
-    private final String password;
-    private final String salt;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Arrays;
 
-    /**
-     *
-     * @param hashedPassword a password hashed by a hashing algorithm.
-     * @param salt the salt used to hash the password.
-     */
-    public Hash(String hashedPassword, String salt) {
+public class Hash {
+    private final byte[] password;
+    private final byte[] salt;
+
+    public Hash(byte[] hashedPassword, byte[] salt) {
         this.password = hashedPassword;
         this.salt = salt;
     }
 
-    public String getPassword() {
+    public static Hash of(String plainTextPassword) {
+        SecureRandom random = new SecureRandom();
+
+        byte[] salt = new byte[16];
+
+        random.nextBytes(salt);
+
+        byte[] hash = hash(plainTextPassword, salt);
+        assert hash != null: "Hash method failed.";
+
+        return new Hash(hash, salt);
+    }
+
+    private static byte[] hash(String plainTextPassword, byte[] salt) {
+        final int HASH_STRENGTH = 65536;
+        final int KEY_LENGTH = 128;
+
+        try {
+            KeySpec spec = new PBEKeySpec(plainTextPassword.toCharArray(), salt, HASH_STRENGTH, KEY_LENGTH);
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+
+            return factory.generateSecret(spec).getEncoded();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public boolean verify(String plainTextPassword) {
+        byte[] obtainedHash = hash(plainTextPassword, salt);
+        return Arrays.equals(password, obtainedHash);
+    }
+
+    public byte[] getPassword() {
         return password;
     }
 
-    public String getSalt() {
+    public byte[] getSalt() {
         return salt;
     }
 }
--- src/test/com/passman/models/HashTest.java
+++ src/test/com/passman/models/HashTest.java
@@ -7,11 +7,37 @@ import static org.junit.Assert.*;
 public class HashTest {
     @Test
     public void shouldConstructAHashCorrectly() {
-        String password = "flyingbananasonfire";
-        String salt = "tastypeppersalt";
+        byte[] password = new byte[16];
+        byte[] salt = new byte[16];
 
         Hash hash = new Hash(password, salt);
 
         assertEquals(password, hash.getPassword());
+    }
+
+    @Test
+    public void ofShouldConstructAValidHash() {
+        String password = "flyingbananasonfire";
+
+        Hash hash = Hash.of(password);
+
+        assertNotNull(hash.getPassword());
+        assertNotNull(hash.getSalt());
+        assertNotEquals(password, hash.getPassword());
+    }
+    
+    @Test
+    public void verifyShouldReturnFalseIfPasswordIsNotCorrect() {
+        Hash hash = Hash.of("song-for-denise");
+
+        assertFalse(hash.verify("song-for-dennis"));
+    }
+
+    @Test
+    public void verifyShouldReturnTrueIfPasswordIsCorrect() {
+        String password = "dennis-villeneuve";
+        Hash hash = Hash.of(password);
+
+        assertTrue(hash.verify(password));
     }
 }
